2984774df3fc313026e45f9670defaea57c51161
wasif <wasif.balol1@gmail.com>
2020-07-02 18:27:24 +0500

    removed key

--- learningUsers/learningUsers/settings.py
+++ learningUsers/learningUsers/settings.py
@@ -22,7 +22,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^w8rws1eekk4#^c3822yyj&xw_z%l(o)lmnpd^^-=784a!!qnl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
